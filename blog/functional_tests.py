8b71996c8b30b9d2b1c84a3e5599db8a99aa1fcc
Abdul-213 <m.abdulhannan.ahmad@gmail.com>
2020-08-29 02:07:32 +0100

    First basic unit test and functional test

---
+++ blog/functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path=r'blog/tests/chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_check_title(self):
+        self.browser.get('http://127.0.0.1:8000/')
+
+        self.assertIn('Abdulhannan Ahmad', self.browser.title)
+        self.fail('Finish the test')
+        
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+        
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from blog.views import resume
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/resume')
+        self.assertEqual(found.func, resume)
