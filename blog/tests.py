6c4450368e12930931e22686ab88d31003cf5cd3
Abdul-213 <m.abdulhannan.ahmad@gmail.com>
2020-08-31 03:35:40 +0100

    updated unit testing to check all html page templates

--- blog/tests.py
+++ blog/tests.py
@@ -4,20 +4,40 @@ from blog.views import resume, home
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-class HomePageTest(TestCase):
+class TemplateTests(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
 
+    # test if templates return correct html 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/home') 
         self.assertTemplateUsed(response, 'blog/home.html')
-    
+
+    def test_post_list_returns_corrent_html(self):
+        response = self.client.get('/blog')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_post_new_returns_correct_html(self):
+        response = self.client.get('/post/new/')
+        self.assertTemplateUsed(response, 'blog/post_edit.html')
+
     def test_resume_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = resume(request)  
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>Resume</title>', html)  
-        self.assertTrue(html.endswith('</html>')) 
+        response = self.client.get('/resume')
+        self.assertTemplateUsed(response, 'blog/resume.html')
+
+    def test_work_experience_new_returns_correct_html(self):
+        response = self.client.get('/work_experience_new')
+        self.assertTemplateUsed(response, 'blog/work_experience_edit.html')
+
+    def test_education_new_returns_correct_html(self):
+        response = self.client.get('/education_new')
+        self.assertTemplateUsed(response, 'blog/education_edit.html')
+    
+    def test_interest_new_returns_correct_html(self):
+        response = self.client.get('/interest_new')
+        self.assertTemplateUsed(response, 'blog/interest_edit.html')
+
+
+
